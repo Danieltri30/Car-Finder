e0759e86567781be01294368c90b0fb48d3a248f
Lucas-Hardeman <lhh22@fsu.edu>
2024-06-16 16:56:04 -0400

    Update app.py
    Changed the index() to allow for filtering based on the inputs, going to office hours to make this work. Uses a filtering cars function that is fire.py

--- app.py
+++ app.py
@@ -2,56 +2,55 @@ from flask import Flask, render_template, request
 import fire
 
 app = Flask(__name__)
-@app.route('/')
-def index():
-    '''if user.loggedIn():
-        return render_template('homepageEmployee.html')'''
-    return render_template('index.html')
 
-@app.route('/createLogin', methods = ['POST', 'GET'])
+
+@app.route('/', methods=['GET'])
+def index():
+    usage = request.args.get('usage', None)
+    transmission = request.args.get('transmission', None)
+    fuel = request.args.get('fuel', None)
+
+    # Call filter_cars with query parameters to get filtered car data
+    filtered_cars = fire.filter_cars(usage=usage, transmission=transmission, fuel=fuel)
+
+    # Render index.html template and pass filtered car data as clist
+    return render_template('index.html', clist=filtered_cars)
+
+@app.route('/createLogin', methods=['POST', 'GET'])
 def createLogin():
     if request.method == 'POST':
         return render_template('login.html')
     else:
         return render_template('createLogin.html')
 
-@app.route('/login', methods = ['POST', 'GET'])
+
+@app.route('/login', methods=['POST', 'GET'])
 def login():
     if request.method == 'POST':
         return render_template('homepageEmployee.html')
     else:
         return render_template('login.html')
 
-@app.route('/carsPosted', methods = ['GET'])
+
+@app.route('/carsPosted', methods=['GET'])
 def carsPosted():
     clist = fire.get_all_cars()
     return render_template('carsPosted.html', clist=clist)
 
-@app.route('/addCar', methods = ['POST', 'GET'])
+
+@app.route('/addCar', methods=['POST', 'GET'])
 def addCar():
-    
     return render_template('addCar.html')
 
-@app.route('/deleteCar', methods = ['POST', 'GET'])
+
+@app.route('/deleteCar', methods=['POST', 'GET'])
 def deleteCar():
     return render_template('deleteCar.html')
 
-@app.route('/updateCar', methods = ['POST', 'GET'])
+
+@app.route('/updateCar', methods=['POST', 'GET'])
 def updateCar():
     return render_template('updateCar.html')
-
-@app.route('/addUser', methods = ['POST', 'GET'])
-def addUser():
-    x = 5       # placeholder until html page is added 
-
-@app.route('/deleteUser', methods = ['POST', 'GET'])
-def deleteUser():
-      x = 5 # placeholder until html page is added
-
-@app.route('/usersPosted', methods = ['GET'])
-def usersPosted():
-    ulist = fire.get_all_users()
-    #return render_template('carsPosted.html', clist=clist)    Placeholder until front end is completed  
 
 
 if __name__ == "__main__":
